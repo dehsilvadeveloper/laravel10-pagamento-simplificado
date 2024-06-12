7442a53229b54dd9e7edc1aa52fc41df379ca482
dehsilva <andre.silva.clinica@gmail.com>
2024-06-12 15:31:48 -0300

    Adicionando classes helper para auxiliar em testes relacionados a Models.

---
+++ tests/TestHelpers/DataTransferObjects/ModelConfigurationAssertionParamsDto.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\TestHelpers\DataTransferObjects;
+
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Mappers\SnakeCaseMapper;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+#[MapInputName(SnakeCaseMapper::class)]
+class ModelConfigurationAssertionParamsDto extends Data
+{
+    public function __construct(
+        public Model $model,
+        public array $fillable = [],
+        public array $hidden = [],
+        public array $guarded = ['*'],
+        public array $visible = [],
+        public array $casts = ['id' => 'int'],
+        public array $dates = ['created_at', 'updated_at'],
+        public string $collectionClass = Collection::class,
+        public ?string $table = null,
+        public string $primaryKey = 'id'
+    ) {
+    }
+}
