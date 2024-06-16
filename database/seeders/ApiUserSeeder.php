041f1172e5dcbd8814c0fb92ea8cd2773fce59e4
dehsilva <andre.silva.clinica@gmail.com>
2024-06-16 11:37:26 -0300

    Melhorando observabilidade da seeder ApiUserSeeder.
    Adicionando commits, rollbacks, verificacao de existencia de registros para inclusao e logs para melhorar o processo e a observabilidade da seeder.

--- database/seeders/ApiUserSeeder.php
+++ database/seeders/ApiUserSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use Throwable;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Domain\ApiUser\Models\ApiUser;
 
 class ApiUserSeeder extends Seeder
@@ -18,15 +20,36 @@ class ApiUserSeeder extends Seeder
     {
         $apiUsers = collect(config('api_users.default'));
 
+        if ($apiUsers->isEmpty()) {
+            Log::info('[ApiUserSeeder] No data was found to be seeded on the table api_users.');
+            return;
+        }
+
         DB::beginTransaction();
 
-        $apiUsers->map(
-            fn($value) => ApiUser::updateOrCreate(
-                ['id' => $value['id']],
-                $value
-            )
-        );
+        try {
+            $apiUsers->map(
+                fn($value) => ApiUser::updateOrCreate(
+                    ['id' => $value['id']],
+                    $value
+                )
+            );
+    
+            DB::commit();
 
-        DB::commit();
+            $this->command->info('Table api_users seeded.');
+        } catch (Throwable $exception) {
+            DB::rollBack();
+
+            Log::error(
+                '[ApiUserSeeder] Error while executing seeder ApiUserSeeder.',
+                [
+                    'error_message' => $exception->getMessage(),
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                    'stack_trace' => $exception->getTrace()
+                ]
+            );
+        }
     }
 }
