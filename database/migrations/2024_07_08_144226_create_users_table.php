95e54bbdff323d00eb372342003abe00f495fa81
dehsilva <andre.silva.clinica@gmail.com>
2024-07-08 14:52:04 -0300

    Removendo migration antiga e adicionando nova para entidade users. Adicionando unsigned em colunas id de migrations.

--- database/migrations/2024_07_08_144226_create_users_table.php
+++ database/migrations/2024_07_08_144226_create_users_table.php
@@ -11,18 +11,28 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('api_users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_unicode_ci';
 
             $table->id()->unsigned();
-            $table->string('name');
+            $table->unsignedBigInteger('user_type_id');
+            $table->string('name', 70);
+            $table->unsignedBigInteger('document_type_id');
+            $table->string('document_number', 14)->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_type_id')
+                  ->references('id')
+                  ->on('user_types');
+
+            $table->foreign('document_type_id')
+                  ->references('id')
+                  ->on('document_types');
         });
     }
 
@@ -31,6 +41,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('api_users');
+        Schema::dropIfExists('users');
     }
 };
