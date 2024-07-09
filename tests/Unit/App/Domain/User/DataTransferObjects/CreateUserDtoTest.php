816175ddd7dff8ac102ad42c799e57a821a9d706
dehsilva <andre.silva.clinica@gmail.com>
2024-07-09 11:50:15 -0300

    Adicionando na classe DtoTestCase um método para validar recebimento de valores de enum inválidos em propriedades de DTO. Implementando DTOs relacionados a entidade users.

---
+++ tests/Unit/App/Domain/User/DataTransferObjects/CreateUserDtoTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Tests\Unit\App\Domain\User\DataTransferObjects;
+
+use Tests\DtoTestCase;
+use Illuminate\Http\Request;
+use App\Domain\DocumentType\Enums\DocumentTypeEnum;
+use App\Domain\User\DataTransferObjects\CreateUserDto;
+use App\Domain\User\Enums\UserTypeEnum;
+
+class CreateUserDtoTest extends DtoTestCase
+{
+    /**
+     * @group dtos
+     * @group user
+     */
+    public function test_can_create_from_array_with_snakecase_keys(): void
+    {
+        $this->runCreationFromSnakecaseArrayAssertions(
+            CreateUserDto::class,
+            [
+                'user_type_id' => UserTypeEnum::COMMON->value,
+                'name' => fake()->name(),
+                'document_number' => fake()->cpf(false),
+                'document_type_id' => DocumentTypeEnum::CPF->value,
+                'email' => fake()->unique()->safeEmail(),
+                'password' => 'defaultpassword'
+            ]
+        );
+    }
+
+    /**
+     * @group dtos
+     * @group user
+     */
+    public function test_can_create_from_array_with_camelcase_keys(): void
+    {
+        $this->runCreationFromSnakecaseArrayAssertions(
+            CreateUserDto::class,
+            [
+                'userTypeId' => UserTypeEnum::COMMON->value,
+                'name' => fake()->name(),
+                'documentNumber' => fake()->cpf(false),
+                'documentTypeId' => DocumentTypeEnum::CPF->value,
+                'email' => fake()->unique()->safeEmail(),
+                'password' => 'defaultpassword'
+            ]
+        );
+    }
+
+    /**
+     * @group dtos
+     * @group user
+     */
+    public function test_cannot_create_from_empty_array(): void
+    {
+        $this->runCreationFromEmptyArrayAssertions(CreateUserDto::class);
+    }
+
+    /**
+     * @group dtos
+     * @group user
+     */
+    public function test_cannot_create_from_array_with_invalid_enum_values(): void
+    {
+        $this->runCreationFromArrayWithInvalidEnumValuesAssertions(
+            CreateUserDto::class,
+            [
+                'user_type_id' => 99,
+                'name' => fake()->name(),
+                'document_number' => fake()->cpf(false),
+                'document_type_id' => 88,
+                'email' => fake()->unique()->safeEmail(),
+                'password' => 'defaultpassword'
+            ]
+        );
+    }
+
+    /**
+     * @group dtos
+     * @group user
+     */
+    public function test_can_create_from_request(): void
+    {
+        $this->runCreationFromRequestAssertions(
+            CreateUserDto::class,
+            Request::create(
+                '/dummy',
+                'POST',
+                [
+                    'user_type_id' => UserTypeEnum::COMMON->value,
+                    'name' => fake()->name(),
+                    'document_number' => fake()->cpf(false),
+                    'document_type_id' => DocumentTypeEnum::CPF->value,
+                    'email' => fake()->unique()->safeEmail(),
+                    'password' => 'defaultpassword'
+                ]
+            )
+        );
+    }
+
+    /**
+     * @group dtos
+     * @group user
+     */
+    public function test_cannot_create_from_empty_request(): void
+    {
+        $this->runCreationFromEmptyRequestAssertions(CreateUserDto::class);
+    }
+
+    /**
+     * @group dtos
+     * @group user
+     */
+    public function test_cannot_create_from_request_with_invalid_values(): void
+    {
+        $this->runCreationFromRequestWithInvalidValuesAssertions(
+            CreateUserDto::class,
+            Request::create(
+                '/dummy',
+                'POST',
+                [
+                    'user_type_id' => 99,
+                    'name' => fake()->name(),
+                    'document_number' => fake()->cpf(false),
+                    'document_type_id' => 88,
+                    'email' => fake()->unique()->safeEmail(),
+                    'password' => 'defaultpassword'
+                ]
+            )
+        );
+    }
+}
