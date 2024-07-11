cdc45ef81c9221f5578800cee20b5b06b4323eec
dehsilva <andre.silva.clinica@gmail.com>
2024-07-11 14:24:27 -0300

    Implementando classes form request para validação de criação e atualização de usuário.

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_type_id' => ['sometimes', 'required', 'integer', 'exists:user_types,id'],
+            'name' => ['sometimes', 'required', 'string', 'max:70'],
+            'document_type_id' => ['sometimes', 'required', 'integer', 'exists:document_types,id'],
+            'document_number' => ['sometimes', 'required', 'string', 'unique:users,document_number'],
+            'email' => ['sometimes', 'required', 'email', 'unique:users,email'],
+            'password' => ['sometimes', 'required', 'string', 'min:8']
+        ];
+    }
+
+    /**
+     * Define body params to be used by the API documentation
+     */
+    public function bodyParameters(): array
+    {
+        return [
+            'user_type_id' => [
+                'description' => 'The type of user.',
+                'example' => 1
+            ],
+            'name' => [
+                'description' => 'The name of the user.',
+                'example' => 'John Doe'
+            ],
+            'document_type_id' => [
+                'description' => 'The type of document that the user has.',
+                'example' => 2
+            ],
+            'document_number' => [
+                'description' => 'The number of the document that the user has.',
+                'example' => '60796747008'
+            ],
+            'email' => [
+                'description' => 'The email address of the user.',
+                'example' => 'john.doe@test.com'
+            ],
+            'password' => [
+                'description' => 'The password of the user.',
+                'example' => 'TDwbC4zvy963xa@#hSEDH'
+            ]
+        ];
+    }
+}
---
+++ tests/Unit/App/Http/Requests/UpdateUserRequestTest.php
@@ -0,0 +1,273 @@
+<?php
+
+namespace Tests\Unit\App\Http\Requests;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\UpdateUserRequest;
+use App\Domain\DocumentType\Enums\DocumentTypeEnum;
+use App\Domain\User\Enums\UserTypeEnum;
+use App\Domain\User\Models\User;
+use Database\Seeders\DocumentTypeSeeder;
+use Database\Seeders\UserTypeSeeder;
+
+class UpdateUserRequestTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(DocumentTypeSeeder::class);
+        $this->seed(UserTypeSeeder::class);
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_pass_with_valid_request(): void
+    {
+        $data = [
+            'user_type_id' => UserTypeEnum::COMMON->value,
+            'document_type_id' => DocumentTypeEnum::CPF->value,
+            'document_number' => fake()->cpf(false),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => fake()->password(12)
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertFalse($validator->fails());
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_pass_without_optional_fields(): void
+    {
+        $data = [];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertFalse($validator->fails());
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_fail_with_non_existent_user_type(): void
+    {
+        $data = [
+            'user_type_id' => 9999,
+            'document_type_id' => DocumentTypeEnum::CPF->value,
+            'document_number' => fake()->cpf(false),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => fake()->password(12)
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+        $this->assertCount(1, $validator->errors());
+        $this->assertTrue($validator->errors()->has('user_type_id'));
+        $this->assertEquals(
+            [
+                'The selected user type id is invalid.'
+            ],
+            $validator->errors()->get('user_type_id')
+        );
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_fail_with_invalid_max_size_name(): void
+    {
+        $data = [
+            'user_type_id' => UserTypeEnum::COMMON->value,
+            'document_type_id' => DocumentTypeEnum::CPF->value,
+            'document_number' => fake()->cpf(false),
+            'name' => fake()->sentence(80),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => fake()->password(12)
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+        $this->assertCount(1, $validator->errors());
+        $this->assertTrue($validator->errors()->has('name'));
+        $this->assertEquals(
+            [
+                'The name field must not be greater than 70 characters.'
+            ],
+            $validator->errors()->get('name')
+        );
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_fail_with_non_existent_document_type(): void
+    {
+        $data = [
+            'user_type_id' => UserTypeEnum::COMMON->value,
+            'document_type_id' => 9999,
+            'document_number' => fake()->cpf(false),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => fake()->password(12)
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+        $this->assertCount(1, $validator->errors());
+        $this->assertTrue($validator->errors()->has('document_type_id'));
+        $this->assertEquals(
+            [
+                'The selected document type id is invalid.'
+            ],
+            $validator->errors()->get('document_type_id')
+        );
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_fail_with_non_unique_document_number(): void
+    {
+        $documentNumber = fake()->cpf(false);
+
+        User::factory()->create([
+            'document_number' => $documentNumber
+        ]);
+
+        $data = [
+            'user_type_id' => UserTypeEnum::COMMON->value,
+            'document_type_id' => DocumentTypeEnum::CPF->value,
+            'document_number' => $documentNumber,
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => fake()->password(12)
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+        $this->assertCount(1, $validator->errors());
+        $this->assertTrue($validator->errors()->has('document_number'));
+        $this->assertEquals(
+            [
+                'The document number has already been taken.'
+            ],
+            $validator->errors()->get('document_number')
+        );
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_fail_with_invalid_email(): void
+    {
+        $data = [
+            'user_type_id' => UserTypeEnum::COMMON->value,
+            'document_type_id' => DocumentTypeEnum::CPF->value,
+            'document_number' => fake()->cpf(false),
+            'name' => fake()->name(),
+            'email' => 'invalid_format_email',
+            'password' => fake()->password(12)
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+        $this->assertCount(1, $validator->errors());
+        $this->assertTrue($validator->errors()->has('email'));
+        $this->assertEquals(
+            [
+                'The email field must be a valid email address.'
+            ],
+            $validator->errors()->get('email')
+        );
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_fail_with_non_unique_email(): void
+    {
+        $email = fake()->unique()->safeEmail();
+
+        User::factory()->create([
+            'email' => $email
+        ]);
+
+        $data = [
+            'user_type_id' => UserTypeEnum::COMMON->value,
+            'document_type_id' => DocumentTypeEnum::CPF->value,
+            'document_number' => fake()->cpf(false),
+            'name' => fake()->name(),
+            'email' => $email,
+            'password' => fake()->password(12)
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+        $this->assertCount(1, $validator->errors());
+        $this->assertTrue($validator->errors()->has('email'));
+        $this->assertEquals(
+            [
+                'The email has already been taken.'
+            ],
+            $validator->errors()->get('email')
+        );
+    }
+
+    /**
+     * @group requests
+     * @group user
+     */
+    public function test_fail_with_invalid_min_size_password(): void
+    {
+        $data = [
+            'user_type_id' => UserTypeEnum::COMMON->value,
+            'document_type_id' => DocumentTypeEnum::CPF->value,
+            'document_number' => fake()->cpf(false),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => 'TDw'
+        ];
+
+        $request = (new UpdateUserRequest())->replace($data);
+        $validator = Validator::make($data, $request->rules(), $request->messages());
+
+        $this->assertTrue($validator->fails());
+        $this->assertCount(1, $validator->errors());
+        $this->assertTrue($validator->errors()->has('password'));
+        $this->assertEquals(
+            [
+                'The password field must be at least 8 characters.'
+            ],
+            $validator->errors()->get('password')
+        );
+    }
+}
