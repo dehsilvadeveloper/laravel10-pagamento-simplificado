7a8e9aeb5734c22cd558c77b6a2f0594fb6269da
dehsilva <andre.silva.clinica@gmail.com>
2024-07-20 13:15:21 -0300

    Removendo rota temporária.

--- routes/api.php
+++ routes/api.php
@@ -17,16 +17,6 @@ use App\Http\Controllers\UserTypeController;
 |
 */
 
-Route::get('/notification-test', function () {
-    //UserCreated::dispatch($user);
-    //$user->notify(new WelcomeNotification());
-
-    $user = \App\Domain\User\Models\User::find(1);
-    $user->notify(new \App\Domain\User\Notifications\WelcomeNotification());
-
-    print 'executed';
-});
-
 Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
 
 Route::prefix('/mailables-previews')->name('mailable-preview.')->group(function () {
