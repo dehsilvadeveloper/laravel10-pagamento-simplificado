aaa7b3a3dcbfedaa701ca5e346238f88c489a4fe
dehsilva <andre.silva.clinica@gmail.com>
2024-07-25 16:32:03 -0300

    Adicionando migration, model e factory para entidade wallet. Atualizando model User com relação com model Wallet.

---
+++ database/migrations/2024_07_25_161316_create_wallets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->id()->unsigned();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('balance', 10, 2)->default(0);
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
